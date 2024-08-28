e864a268072b7403bf69bd1ad9ef4b4da7239ad9
mottakinrahat <rahatchowdhury661@gmail.com>
2024-08-28 09:22:20 +0600

    backEndDone

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,62 @@
-import { Server } from 'http';
-import config from './config';
-import { Server as SocketIOServer } from 'socket.io';
-import app, { corsOptions } from './app/app';
-import { chatServices } from './app/modules/chat/chat.services';
+import { Server } from "http";
+import config from "./config";
+import { Server as SocketIOServer } from "socket.io";
+import app, { corsOptions } from "./app/app";
+import { chatServices } from "./app/modules/chat/chat.services";
 async function main() {
-    const server: Server =app.listen(config.port, () => {
-        console.log("Sever is running on port ", config.port);
+  const server: Server = app.listen(config.port, () => {
+    console.log("Sever is running on port ", config.port);
+  });
+  const io = new SocketIOServer(server, {
+    cors: corsOptions,
+  });
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("joinRoom", async (roomId) => {
+      try {
+        socket.join(roomId);
+        console.log(`Socket ${socket.id} joined room ${roomId}`);
+        const messages = await chatServices.getMessagesByChatroomIntoDB(roomId);
+        socket.emit("loadMessages", messages);
+      } catch (error) {
+        console.error(`Error joining room ${roomId}:`, error);
+      }
     });
-    const io = new SocketIOServer(server, {
-        cors: corsOptions
-    });
-    io.on("connection", (socket) => {
-        console.log("A user connected:", socket.id);
     
-        socket.on("joinRoom", async (roomId) => {
-          socket.join(roomId);
-          console.log(`Socket ${socket.id} joined room ${roomId}`);
-          const messages = await chatServices.getMessagesByChatroomIntoDB(roomId);
-          socket.emit("loadMessages", messages);
-        });
-    
-        socket.on("sendMessage", async (data) => {
-          console.log("Message received:", data);
-          const { chatroomId, senderId, content } = data;
-          const message = await chatServices.createMessageIntoDB(chatroomId, senderId, content);
-          io.to(chatroomId).emit("receiveMessage", message);
-        });
-    
-        socket.on("disconnect", () => {
-          console.log("User disconnected:", socket.id);
-        });
-    });
-    const exitHandler = () => {
-        if (server) {
-            server.close(() => {
-                console.info("Server closed!")
-            })
-        }
-        process.exit(1);
-    };
-    process.on('uncaughtException', (error) => {
-        console.log(error);
-        exitHandler();
+
+    socket.on("sendMessage", async (data) => {
+      console.log("Message received:", data);
+      const { chatroomId, senderId, content } = data;
+      const message = await chatServices.createMessageIntoDB(
+        chatroomId,
+        senderId,
+        content
+      );
+      io.to(chatroomId).emit("receiveMessage", message);
     });
 
-    process.on('unhandledRejection', (error) => {
-        console.log(error);
-        exitHandler();
-    })
-};
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+}
 
 main();
