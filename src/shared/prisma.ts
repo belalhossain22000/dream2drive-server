89d57c816685ec71de65ee899dac2003761facc7
Shahed007 <sk0178397@gmail.com>
2024-09-04 15:15:17 +0600

    updated code.

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,35 +1,46 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log: [
-        {
-            emit: 'event',
-            level: 'query',
-        },
-        {
-            emit: 'event',
-            level: 'error',
-        },
-        {
-            emit: 'event',
-            level: 'info',
-        },
-        {
-            emit: 'event',
-            level: 'warn',
-        },
-    ],
-})
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
 
-prisma.$on('query', (e) => {
-    console.log("-------------------------------------------")
-    console.log('Query: ' + e.query);
-    console.log("-------------------------------------------")
-    console.log('Params: ' + e.params)
-    console.log("-------------------------------------------")
-    console.log('Duration: ' + e.duration + 'ms')
-    console.log("-------------------------------------------")
-})
+async function connectDB() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+}
+
+connectDB();
+
+// prisma.$on("query", (e) => {
+//   console.log("-------------------------------------------");
+//   console.log("Query: " + e.query);
+//   console.log("-------------------------------------------");
+//   console.log("Params: " + e.params);
+//   console.log("-------------------------------------------");
+//   console.log("Duration: " + e.duration + "ms");
+//   console.log("-------------------------------------------");
+// });
 
 // prisma.$on('warn', (e) => {
 //     console.log(e)
