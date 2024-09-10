fe671bb212055bffdb1f03e63869b1b5ab6f656b
belalhossain22000 <belalhossain22000@gmail.com>
2024-09-10 15:52:53 +0600

    modifythe image upload system

---
+++ src/helpars/fileUploaderVps.ts
@@ -0,0 +1,33 @@
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+// Create directory if it doesn't exist
+const storagePath = path.join(process.cwd(), 'uploads');
+if (!fs.existsSync(storagePath)) {
+  fs.mkdirSync(storagePath, { recursive: true });
+}
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, storagePath); // Save files to the 'uploads' folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`); // Use timestamp + original filename to avoid name conflicts
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Middleware to upload single/multiple files
+const uploadSingle = upload.single('carImage');
+const uploadMultiple = upload.fields([
+  { name: 'singleImage', maxCount: 10 },
+  { name: 'galleryImage', maxCount: 100 },
+]);
+
+export const fileUploader = {
+  uploadSingle,
+  uploadMultiple,
+};
