e5ee48f98c35cf108416a1b9d64180d843c399ea
belalhossain22000 <belalhossain22000@gmail.com>
2024-09-11 09:14:54 +0600

    modify

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -13,7 +13,6 @@ export interface ICloudinaryResult {
 const createProduct = catchAsync(async (req: Request, res: Response) => {
   const userId = req.user.id;
   const files = req.files as any;
-  console.log(files);
 
   if (!files || files.length === 0) {
     return res.status(400).send({ message: "No files uploaded" });
