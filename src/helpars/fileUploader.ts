ed79e6ab4ce0306259c875e2f8063461a4454f53
belalhossain22000 <belalhossain22000@gmail.com>
2024-09-11 09:20:46 +0600

    change path

--- src/helpars/fileUploader.ts
+++ src/helpars/fileUploader.ts
@@ -12,7 +12,7 @@ import config from "../config";
 // });
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(process.cwd(), "/uploads"));
+    cb(null, path.join(process.cwd(), "/var/www/uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
