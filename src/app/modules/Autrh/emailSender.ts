7e67d7405017a76e13e2ca9dc0b1d21aa62c3d75
belalhossain22000 <belalhossain22000@gmail.com>
2024-09-14 12:47:56 +0600

    change configration

--- src/app/modules/Autrh/emailSender.ts
+++ src/app/modules/Autrh/emailSender.ts
@@ -4,26 +4,32 @@ import config from "../../../config";
 const emailSender = async (subject: string, email: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: "mail.privateemail.com",
-    port: 465 , 
-    secure: true, 
+    port: 465,
+    secure: true,
     auth: {
       user: "info@dream2drive.com.au",
-      pass:"dream2drive123@",
+      pass: "dream2drive123@",
     },
     tls: {
       rejectUnauthorized: false,
     },
   });
 
-  const info = await transporter.sendMail({
-    from:  `"Collecting Cars" <${config.emailSender.email}>`, 
-    to: email,
-    subject: `${subject}`, 
-   
-    html, 
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: `"Collecting Cars" <${config.emailSender.email}>`,
+      to: email,
+      subject: `${subject}`,
 
-  console.log("Message sent: %s", info.messageId);
+      html,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Response envelope: ", info.envelope);
+    console.log("Response message: ", info.messageId);
+  } catch (error) {
+    console.error("Error sending email: ", error);
+  }
 };
 
 export default emailSender;
