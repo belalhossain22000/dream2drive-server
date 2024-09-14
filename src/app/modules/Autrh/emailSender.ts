bc4be2f99176ea947fa86734e85f01473711412f
Shahed007 <sk0178397@gmail.com>
2024-09-14 16:22:05 +0600

    updated code .

--- src/app/modules/Autrh/emailSender.ts
+++ src/app/modules/Autrh/emailSender.ts
@@ -5,13 +5,13 @@ const emailSender = async (subject: string, email: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: "mail.privateemail.com",
     port: 465,
-    secure: true,
+    secure: true, // true for 465, false for 587
     auth: {
       user: config.emailSender.email,
       pass: config.emailSender.app_pass,
     },
     tls: {
-      rejectUnauthorized: true,
+      rejectUnauthorized: false, // You can set this to false for Namecheap
     },
   });
 
@@ -19,10 +19,12 @@ const emailSender = async (subject: string, email: string, html: string) => {
     const info = await transporter.sendMail({
       from: `"Dream2Drive" <${config.emailSender.email}>`,
       to: email,
-      subject: `${subject}`,
-      html,
-      replyTo: email,
+      subject: subject,
+      html: html,
+      // Remove or set a valid Reply-To
     });
+
+    console.log("Email sent: ", info.messageId); // Useful for tracking the email
   } catch (error) {
     console.error("Error sending email: ", error);
   }
