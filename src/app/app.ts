9fa9c84871fcad51d70179718b1d196db53c8b72
mottakinrahat <rahatchowhdury661@gmail.com>
2024-10-12 22:58:16 +0600

    corsDone

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,7 +16,8 @@ export const corsOptions = {
     "https://dream-2-drive.vercel.app",
     "https://dream2drive.com.au",
     "dream2drive.com.au",
-    "https://dream2drive.vercel.app"
+    "https://dream2drive.vercel.app",
+    "https://dream2drive-mocha.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
